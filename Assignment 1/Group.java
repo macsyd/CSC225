d746213ee2ccba04a5fd6df89b35598c3e6d449d
Sydney Macdonald <macdonaldpsydney@gmail.com>
2023-06-09 18:30:52 -0700

    Initialize repo

---
+++ Assignment 1/Group.java
@@ -0,0 +1,23 @@
+public class Group {
+
+  
+  public void addFrog(Frog f) {
+  }
+
+  public int size() {
+  }
+
+
+  public Frog get(int i) {
+  }
+
+  public Group[] halfGroups() {
+  }
+
+  @Override
+  public String toString() {
+  }
+
+  public static boolean FrogEquals(Group g1, Group g2) {
+  }
+}
